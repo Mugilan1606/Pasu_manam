595364aaac018c605fe6fb304ce5c201df04e577
Mugilan v <mugilr2@gmail.com>
2026-03-12 07:40:05 +0530

    fix allowed hosts

--- pasumanam/settings.py
+++ pasumanam/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ap^b=frg1co%#hg#v8d3e*)+ew!01dyv4wpydoa7k$f%leke_z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
